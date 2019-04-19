f3a2891ccdcfc5807a49cea6e15c0dc0901e97a0
crich18 <49662125+crich18@users.noreply.github.com>
2019-04-19 03:17:57 -0400

    KNN, Multinomial LogReg, XGBoost.
    Dont forget to change the path for the .mat file. Still need SVM, but shouldn't been too hard. KNN has structure to optimize a single parameter, but MLR and XGB don't need it. Also, we need calc the other metrics and generate any plots we want for the poster/report.
    
    Note: I wasn't able to successfully install xgBoost, so I couldn't check that it actually runs bug free.

--- Bench_Model_Imp.py
+++ Bench_Model_Imp.py
@@ -1,97 +1,158 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 16 15:45:29 2019
+Created on Thu Apr 18 16:48:06 2019
 
-@author: Shafaat
+@author: Cyrus
 """
 
+
 import scipy.io as spio
+import scipy.stats as stats
 import math
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold
-from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.model_selection import KFold, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier  
-from sklearn.metrics import classification_report, confusion_matrix  
+from sklearn.metrics import classification_report, confusion_matrix , accuracy_score
 from sklearn.model_selection import cross_val_score
-
+from xgboost.sklearn import XGBClassifier
 
 matfile = "/Users/Cyrus/Documents/Classes/ECE 6254/Project/slpdb_additional_features.mat"
 data =spio.loadmat(matfile)
-raw_y = pd.DataFrame(data['y'])
-raw_X = pd.DataFrame(data['x'])
-raw_Xy = [raw_y,raw_X]
-result_Xy = pd.concat(raw_Xy,axis=1).dropna()
-y = result_Xy.iloc[:,0]
-X = result_Xy.iloc[:,1:]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.20, random_state=42)
-scaler = StandardScaler()  
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)  
-X_test = scaler.transform(X_test)
+nfolds = 10
+seed = 42
+feat_ns = [27,14,7,4]
+raw_y = data["y"]
+raw_X = data["x"]
+mask = ~np.any(np.isnan(raw_X), axis=1)
+y = raw_y[mask]
+X = raw_X[mask]
 
 
-######################################################################
-
-# creating odd list of K for KNN
+########## KNN ###############
 neighbors = list(range(1,50, 2))
-nfeat_list = [14,7,4]
+opt_k_list = -1*np.ones((len(feat_ns),nfolds))
+acc_list_knn = -1*np.ones((len(feat_ns),nfolds))
 
-# empty list that will hold cv sclenores
-cv_scores = np.zeros(( len(nfeat_list),len(neighbors) ))
-
-
-
-
-# perform 10 fold cross validations
-### WOrking on using PCA for cross validation:
-kfcv = KFold(n_splits=10, shuffle = True, random_state=6254)
-for i in range(0,len(nfeat_list)):
-    for train_idx, test_idx in kfcv.split(X_train, y_train):
-        Xcv_train, Xcv_test = X_train[train_idx], X_train[test_idx]
-        ycv_train, ycv_test = y_train[train_idx], y_train[test_idx]
-        pca = PCA(n_components = nfeat_list[i])
-        post_extract = pca.fit_transform(Xcv_train, ycv_train)
-        for j in range(0, len(neighbors)):
-            knn = KNeighborsClassifier(n_neighbors=neighbors[j])
-            knn.fit(pca.transform(Xcv_train), ycv_train)
-            accuracy = knn.predict(ycv_train)
+kfcv = KFold(nfolds, True, seed)
+fold_no = 0
+##CV loop
+for maj_idx, min_idx in kfcv.split(X):
+    X_maj, X_min = X[maj_idx], X[min_idx]
+    y_maj, y_min = y[maj_idx], y[min_idx]
+    #Split the major fold into validation and train sets
     
-########## KNN #####################
+    ### Loop through values for # of features
+    for n_idx in range(0,len(feat_ns)):
+        X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size= 1/nfolds, random_state=seed)
+        
+        feat_selector = PCA(n_components = feat_ns[n_idx])
+        feat_selector.fit(X_train, y_train)
+        X_train = feat_selector.transform(X_train)
+        X_valid = feat_selector.transform(X_valid)
+        #X_min = feat_selector.transform(X_min)
+        
+        
+        record_acc = -1
+        record_k = -1
+        record_model = None
+        cand = -1
+        for k in neighbors:
+            knn = KNeighborsClassifier(n_neighbors = k)
+            knn.fit(X_train, y_train)
+            cand = accuracy_score(y_valid, knn.predict(X_valid))
+            if (cand > record_acc):
+                record_acc = cand
+                record_k = k
+                record_model = knn
+        print("select k of %d, with accuracy %f" % (record_k, record_acc))
+        opt_k_list[n_idx][fold_no] = record_k
+        acc_list_knn[n_idx][fold_no] = accuracy_score(y_min,record_model.predict(feat_selector.transform(X_min)))
+    fold_no+=1
+print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_knn, axis =1))
+print("mode of opt_k for each dimensionality of feature space is  ", stats.mode(opt_k_list, axis = 1).mode)
 
-# Questions:
-### How do we determine the number of features to selction
-### we need to perform feature selection for each fold, so I don't think we can use
-### the cross_val_score() method as we are below.
+############ Multinomial Logistic Regression ###################
+acc_list_mlr = -1*np.ones((len(feat_ns),nfolds))
 
-for k in neighbors:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')
-    cv_scores.append(scores.mean())
+kfcv = KFold(nfolds, True, seed)
+fold_no = 0
+##CV loop
+for maj_idx, min_idx in kfcv.split(X):
+    X_maj, X_min = X[maj_idx], X[min_idx]
+    y_maj, y_min = y[maj_idx], y[min_idx]
+    #Split the major fold into validation and train sets
+    
+    ### Loop through values for # of features
+    for n_idx in range(0,len(feat_ns)):
+        X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size= 1/nfolds, random_state=seed)
+        
+        feat_selector = PCA(n_components = feat_ns[n_idx])
+        feat_selector.fit(X_train, y_train)
+        X_train = feat_selector.transform(X_train)
+        X_valid = feat_selector.transform(X_valid)
+        
+        mlr = linear_model.LogisticRegression(multi_class = "multinomial", solver = "newton-cg")
+        mlr.fit(X_train, y_train)
+        acc_list_mlr[n_idx][fold_no] = accuracy_score(y_min,mlr.predict(feat_selector.transform(X_min)))
+        print(acc_list_mlr[n_idx][fold_no])
+#        record_acc = -1
+#        record_k = -1
+#        record_model = None
+#        cand = -1
+#        for k in neighbors:
+#            knn = KNeighborsClassifier(n_neighbors = k)
+#            knn.fit(X_train, y_train)
+#            cand = accuracy_score(y_valid, knn.predict(X_valid))
+#            if (cand > record_acc):
+#                record_acc = cand
+#                record_k = k
+#                record_model = knn
+#        print("select k of %d, with accuracy %f" % (record_k, record_acc))
+#        opt_k_list[n_idx][fold_no] = record_k
+#        acc_list_knn[n_idx][fold_no] = accuracy_score(y_min,record_model.predict(feat_selector.transform(X_min)))
+    fold_no+=1
+    print("new fold")
+print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_mlr, axis =1))
 
-# changing to misclassification error
-MSE = [1 - x for x in cv_scores]
 
-# determining best k
-optimal_k = neighbors[MSE.index(min(MSE))]
-print ("The optimal number of neighbors is %d" % optimal_k)
+################### XgBoost ############################
+acc_list_xgb = [-1]*nfolds
 
-# plot misclassification error vs k
-plt.plot(neighbors, MSE)
-plt.xlabel('Number of Neighbors K')
-plt.ylabel('Misclassification Error')
-plt.show()
+params = {
+    'objective': 'multi:softprob',  # error evaluation for multiclass training
+    'max_depth': 3,
+    'learning_rate': 1.0,  # the maximum depth of each tree
+    'silent': 1.0,         # logging mode - quiet
+    'n_estimators': 100    # no. of trees
+}
+
+kfcv = KFold(nfolds, True, seed)
+fold_no = 0
+##CV loop
+for maj_idx, min_idx in kfcv.split(X):
+    X_maj, X_min = X[maj_idx], X[min_idx]
+    y_maj, y_min = y[maj_idx], y[min_idx]
+    
+    
+    X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size= 1/nfolds, random_state=seed)
+    feat_selector = PCA(n_components = feat_ns[n_idx])
+    feat_selector.fit(X_train, y_train)
+    X_train = feat_selector.transform(X_train)
+    X_valid = feat_selector.transform(X_valid)
+    
+    xgb = XGBClassifier(**params).fit(X_train, y_train)
+    acc_list_xgb[fold_no] = accuracy_score(y_min,xgb.predict(X_min))
+    print(acc_list_xgb[fold_no])
+    fold_no+=1
+    print("new fold")
+print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_xgb, axis =1))
 
 
-classifier = KNeighborsClassifier(n_neighbors=5)  
-classifier.fit(X_train, y_train)  
-y_pred = classifier.predict(X_test)  
-
-
-print(confusion_matrix(y_test, y_pred))  
-print(classification_report(y_test, y_pred))  
