07c1e294e5cee49a6fa7ad1f8586f55db424d5f3
zifan <jiangstefan@gmail.com>
2019-04-19 22:59:09 -0400

    add kappa&SVM

--- Bench_Model_Imp.py
+++ Bench_Model_Imp.py
@@ -5,7 +5,7 @@ Created on Thu Apr 18 16:48:06 2019
 
 @author: Cyrus
 """
-
+from __future__ import division
 
 import scipy.io as spio
 import scipy.stats as stats
@@ -13,146 +13,217 @@ import math
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
+from sklearn.svm import SVC
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.model_selection import KFold, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsClassifier  
-from sklearn.metrics import classification_report, confusion_matrix , accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.model_selection import cross_val_score
 from xgboost.sklearn import XGBClassifier
+from sklearn.metrics import cohen_kappa_score
 
-matfile = "/Users/Cyrus/Documents/Classes/ECE 6254/Project/slpdb_additional_features.mat"
-data =spio.loadmat(matfile)
+matfile = "data/slpdb_additional_features.mat"
+data = spio.loadmat(matfile)
 nfolds = 10
 seed = 42
-feat_ns = [27,14,7,4]
+feat_ns = [27, 14, 7, 4]
 raw_y = data["y"]
 raw_X = data["x"]
 mask = ~np.any(np.isnan(raw_X), axis=1)
 y = raw_y[mask]
 X = raw_X[mask]
+clfname = 'svm'  # knn,svm,MLR,xgboost
 
+if clfname == 'knn':
+    ########## KNN ###############
+    neighbors = list(range(1, 50, 2))
+    opt_k_list = -1 * np.ones((len(feat_ns), nfolds))
+    acc_list_knn = -1 * np.ones((len(feat_ns), nfolds))
+    auc_list_knn = -1 * np.ones((len(feat_ns), nfolds))
+    kappa_list_knn = -1 * np.ones((len(feat_ns), nfolds))
 
-########## KNN ###############
-neighbors = list(range(1,50, 2))
-opt_k_list = -1*np.ones((len(feat_ns),nfolds))
-acc_list_knn = -1*np.ones((len(feat_ns),nfolds))
+    kfcv = KFold(nfolds, True, seed)
+    fold_no = 0
+    ##CV loop
+    for maj_idx, min_idx in kfcv.split(X):
+        X_maj, X_min = X[maj_idx], X[min_idx]
+        y_maj, y_min = y[maj_idx], y[min_idx]
+        # Split the major fold into validation and train sets
 
-kfcv = KFold(nfolds, True, seed)
-fold_no = 0
-##CV loop
-for maj_idx, min_idx in kfcv.split(X):
-    X_maj, X_min = X[maj_idx], X[min_idx]
-    y_maj, y_min = y[maj_idx], y[min_idx]
-    #Split the major fold into validation and train sets
-    
-    ### Loop through values for # of features
-    for n_idx in range(0,len(feat_ns)):
-        X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size= 1/nfolds, random_state=seed)
-        
-        feat_selector = PCA(n_components = feat_ns[n_idx])
-        feat_selector.fit(X_train, y_train)
-        X_train = feat_selector.transform(X_train)
-        X_valid = feat_selector.transform(X_valid)
-        #X_min = feat_selector.transform(X_min)
-        
-        
-        record_acc = -1
-        record_k = -1
-        record_model = None
-        cand = -1
-        for k in neighbors:
-            knn = KNeighborsClassifier(n_neighbors = k)
-            knn.fit(X_train, y_train)
-            cand = accuracy_score(y_valid, knn.predict(X_valid))
-            if (cand > record_acc):
-                record_acc = cand
-                record_k = k
-                record_model = knn
-        print("select k of %d, with accuracy %f" % (record_k, record_acc))
-        opt_k_list[n_idx][fold_no] = record_k
-        acc_list_knn[n_idx][fold_no] = accuracy_score(y_min,record_model.predict(feat_selector.transform(X_min)))
-    fold_no+=1
-print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_knn, axis =1))
-print("mode of opt_k for each dimensionality of feature space is  ", stats.mode(opt_k_list, axis = 1).mode)
+        ### Loop through values for # of features
+        for n_idx in range(0, len(feat_ns)):
+            X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size=1 / nfolds, random_state=seed)
 
-############ Multinomial Logistic Regression ###################
-acc_list_mlr = -1*np.ones((len(feat_ns),nfolds))
+            feat_selector = PCA(n_components=feat_ns[n_idx])
+            feat_selector.fit(X_train, y_train)
+            X_train = feat_selector.transform(X_train)
+            X_valid = feat_selector.transform(X_valid)
+            # X_min = feat_selector.transform(X_min)
 
-kfcv = KFold(nfolds, True, seed)
-fold_no = 0
-##CV loop
-for maj_idx, min_idx in kfcv.split(X):
-    X_maj, X_min = X[maj_idx], X[min_idx]
-    y_maj, y_min = y[maj_idx], y[min_idx]
-    #Split the major fold into validation and train sets
-    
-    ### Loop through values for # of features
-    for n_idx in range(0,len(feat_ns)):
-        X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size= 1/nfolds, random_state=seed)
-        
-        feat_selector = PCA(n_components = feat_ns[n_idx])
-        feat_selector.fit(X_train, y_train)
-        X_train = feat_selector.transform(X_train)
-        X_valid = feat_selector.transform(X_valid)
-        
-        mlr = linear_model.LogisticRegression(multi_class = "multinomial", solver = "newton-cg")
-        mlr.fit(X_train, y_train)
-        acc_list_mlr[n_idx][fold_no] = accuracy_score(y_min,mlr.predict(feat_selector.transform(X_min)))
-        print(acc_list_mlr[n_idx][fold_no])
-#        record_acc = -1
-#        record_k = -1
-#        record_model = None
-#        cand = -1
-#        for k in neighbors:
-#            knn = KNeighborsClassifier(n_neighbors = k)
-#            knn.fit(X_train, y_train)
-#            cand = accuracy_score(y_valid, knn.predict(X_valid))
-#            if (cand > record_acc):
-#                record_acc = cand
-#                record_k = k
-#                record_model = knn
-#        print("select k of %d, with accuracy %f" % (record_k, record_acc))
-#        opt_k_list[n_idx][fold_no] = record_k
-#        acc_list_knn[n_idx][fold_no] = accuracy_score(y_min,record_model.predict(feat_selector.transform(X_min)))
-    fold_no+=1
-    print("new fold")
-print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_mlr, axis =1))
+            record_acc = -1
+            record_k = -1
+            record_model = None
+            cand = -1
+            for k in neighbors:
+                knn = KNeighborsClassifier(n_neighbors=k)
+                knn.fit(X_train, y_train)
+                cand = accuracy_score(y_valid, knn.predict(X_valid))
+                if (cand > record_acc):
+                    record_acc = cand
+                    record_k = k
+                    record_model = knn
+            print("select k of %d, with accuracy %f" % (record_k, record_acc))
+            opt_k_list[n_idx][fold_no] = record_k
+            predicted = record_model.predict(feat_selector.transform(X_min))
+            acc_list_knn[n_idx][fold_no] = accuracy_score(y_min, predicted)
+            kappa_list_knn[n_idx][fold_no] = cohen_kappa_score(y_min, predicted)
 
+        fold_no += 1
+    print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_knn, axis=1), ' std:'
+          , np.std(acc_list_knn, axis=1))
+    print("mode of opt_k for each dimensionality of feature space is  ", stats.mode(opt_k_list, axis=1).mode)
+    print("mean cohen's Kappa for each dimensionality of feature space is ", np.mean(kappa_list_knn, axis=1))
 
-################### XgBoost ############################
-acc_list_xgb = [-1]*nfolds
+if clfname == 'MLR':
+    ############ Multinomial Logistic Regression ###################
+    acc_list_mlr = -1 * np.ones((len(feat_ns), nfolds))
+    kappa_list_mlr = -1 * np.ones((len(feat_ns), nfolds))
 
-params = {
-    'objective': 'multi:softprob',  # error evaluation for multiclass training
-    'max_depth': 3,
-    'learning_rate': 1.0,  # the maximum depth of each tree
-    'silent': 1.0,         # logging mode - quiet
-    'n_estimators': 100    # no. of trees
-}
+    kfcv = KFold(nfolds, True, seed)
+    fold_no = 0
+    ##CV loop
+    for maj_idx, min_idx in kfcv.split(X):
+        X_maj, X_min = X[maj_idx], X[min_idx]
+        y_maj, y_min = y[maj_idx], y[min_idx]
+        # Split the major fold into validation and train sets
 
-kfcv = KFold(nfolds, True, seed)
-fold_no = 0
-##CV loop
-for maj_idx, min_idx in kfcv.split(X):
-    X_maj, X_min = X[maj_idx], X[min_idx]
-    y_maj, y_min = y[maj_idx], y[min_idx]
-    
-    
-    X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size= 1/nfolds, random_state=seed)
-    feat_selector = PCA(n_components = feat_ns[n_idx])
-    feat_selector.fit(X_train, y_train)
-    X_train = feat_selector.transform(X_train)
-    X_valid = feat_selector.transform(X_valid)
-    
-    xgb = XGBClassifier(**params).fit(X_train, y_train)
-    acc_list_xgb[fold_no] = accuracy_score(y_min,xgb.predict(X_min))
-    print(acc_list_xgb[fold_no])
-    fold_no+=1
-    print("new fold")
-print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_xgb, axis =1))
+        ### Loop through values for # of features
+        for n_idx in range(0, len(feat_ns)):
+            X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size=1 / nfolds, random_state=seed)
 
+            feat_selector = PCA(n_components=feat_ns[n_idx])
+            feat_selector.fit(X_train, y_train)
+            X_train = feat_selector.transform(X_train)
+            X_valid = feat_selector.transform(X_valid)
 
+            mlr = linear_model.LogisticRegression(multi_class="multinomial", solver="newton-cg")
+            mlr.fit(X_train, y_train)
+            predicted = mlr.predict(feat_selector.transform(X_min))
+            acc_list_mlr[n_idx][fold_no] = accuracy_score(y_min, predicted)
+            kappa_list_mlr[n_idx][fold_no] = cohen_kappa_score(y_min, predicted)
+            print(acc_list_mlr[n_idx][fold_no])
+        #        record_acc = -1
+        #        record_k = -1
+        #        record_model = None
+        #        cand = -1
+        #        for k in neighbors:
+        #            knn = KNeighborsClassifier(n_neighbors = k)
+        #            knn.fit(X_train, y_train)
+        #            cand = accuracy_score(y_valid, knn.predict(X_valid))
+        #            if (cand > record_acc):
+        #                record_acc = cand
+        #                record_k = k
+        #                record_model = knn
+        #        print("select k of %d, with accuracy %f" % (record_k, record_acc))
+        #        opt_k_list[n_idx][fold_no] = record_k
+        #        acc_list_knn[n_idx][fold_no] = accuracy_score(y_min,record_model.predict(feat_selector.transform(X_min)))
+        fold_no += 1
+        print("new fold")
+    print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_mlr, axis=1))
+    print("mean cohen's Kappa for each dimensionality of feature space is ", np.mean(kappa_list_mlr, axis=1))
+
+if clfname == 'svm':
+    ############ SVM ###################
+    acc_list_svc = -1 * np.ones((len(feat_ns), nfolds))
+    opt_c_list = -1 * np.ones((len(feat_ns), nfolds))
+    kappa_list_svc = -1 * np.ones((len(feat_ns), nfolds))
+
+    kfcv = KFold(nfolds, True, seed)
+    fold_no = 0
+    Cs = np.logspace(0, 2, 10)
+    Cs = np.logspace(0, 0, 1)
+    ##CV loop
+    for maj_idx, min_idx in kfcv.split(X):
+        X_maj, X_min = X[maj_idx], X[min_idx]
+        y_maj, y_min = y[maj_idx], y[min_idx]
+        # Split the major fold into validation and train sets
+
+        ### Loop through values for # of features
+        for n_idx in range(0, len(feat_ns)):
+            X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size=1 / nfolds, random_state=seed)
+
+            feat_selector = PCA(n_components=feat_ns[n_idx])
+            feat_selector.fit(X_train, y_train)
+            X_train = feat_selector.transform(X_train)
+            X_valid = feat_selector.transform(X_valid)
+
+            record_acc = -1
+            record_c = -1
+            record_model = None
+            cand = -1
+            for c in Cs:
+                clf = SVC(C=c, kernel='rbf')
+                clf.fit(X_train, y_train)
+                cand = accuracy_score(y_valid, clf.predict(X_valid))
+                if (cand > record_acc):
+                    record_acc = cand
+                    record_c = c
+                    record_model = clf
+            print("select c of %f, with accuracy %f" % (record_c, record_acc))
+            opt_c_list[n_idx][fold_no] = record_c
+            predicted = record_model.predict(feat_selector.transform(X_min))
+            acc_list_svc[n_idx][fold_no] = accuracy_score(y_min, predicted)
+            kappa_list_svc[n_idx][fold_no] = cohen_kappa_score(y_min, predicted)
+        fold_no += 1
+        print("new fold")
+    print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_svc, axis=1))
+    print("mode of opt_c for each dimensionality of feature space is  ", stats.mode(opt_c_list, axis=1).mode)
+    print("mean cohen's Kappa for each dimensionality of feature space is ", np.mean(kappa_list_svc, axis=1))
+
+if clfname == 'xgboost':
+    ################### XgBoost ############################
+    acc_list_xgb = [-1] * nfolds
+    kappa_list_xgb = [-1] * nfolds
+
+    params = {
+        'objective': 'multi:softprob',  # error evaluation for multiclass training
+        'max_depth': 3,
+        'learning_rate': 1.0,  # the maximum depth of each tree
+        'silent': 1.0,  # logging mode - quiet
+        'n_estimators': 100  # no. of trees
+    }
+
+    kfcv = KFold(nfolds, True, seed)
+    fold_no = 0
+    ##CV loop
+    for maj_idx, min_idx in kfcv.split(X):
+        X_maj, X_min = X[maj_idx], X[min_idx]
+        y_maj, y_min = y[maj_idx], y[min_idx]
+
+        X_train, X_valid, y_train, y_valid = train_test_split(X_maj, y_maj, test_size=1 / nfolds, random_state=seed)
+        # feat_selector = PCA(n_components=feat_ns[n_idx])
+        # feat_selector.fit(X_train, y_train)
+        # X_train = feat_selector.transform(X_train)
+        # X_valid = feat_selector.transform(X_valid)
+        params = {
+            'objective': 'multi:softprob',  # error evaluation for multiclass training
+            'max_depth': 3,
+            'learning_rate': 1.0,  # the maximum depth of each tree
+            'silent': 1,  # logging mode - quiet
+            'n_estimators': 100  # no. of trees
+        }
+
+        xgb = XGBClassifier(**params).fit(X_train, y_train)
+
+        predicted = xgb.predict(X_min)
+        acc_list_xgb[fold_no] = accuracy_score(y_min, xgb.predict(X_min))
+        kappa_list_xgb[fold_no] = cohen_kappa_score(y_min, predicted)
+        print(acc_list_xgb[fold_no])
+        fold_no += 1
+        print("new fold")
+    print("mean performance for each dimensionality of feature space is ", np.mean(acc_list_xgb, axis=0))
+    print("mean cohen's Kappa for each dimensionality of feature space is ", np.mean(kappa_list_xgb, axis=0))
--- train_cnn.py
+++ train_cnn.py
@@ -14,6 +14,7 @@ from torch.autograd import Variable
 from scipy.io import loadmat
 from NetModels import *
 from cpc import cpc
+from sklearn.metrics import accuracy_score, cohen_kappa_score
 
 rdata = loadmat('data/slpdb_cnn_dataset.mat')
 
@@ -21,16 +22,17 @@ test_flg = False
 bs = 512
 total_epoch = 50
 learning_rate = 0.01
-best_val_acc = 0  
+best_val_acc = 0
 best_val_acc_epoch = 0
 print(torch.cuda.get_device_name(0))
 print('==> Preparing data..')
 
 transform_train = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),])
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(), ])
 transform_test = transforms.Compose([
-        transforms.ToTensor()])
+    transforms.ToTensor()])
+
 
 def train(epoch):
     epoch_start = time.time()
@@ -39,11 +41,11 @@ def train(epoch):
     train_loss = 0
     correct = 0
     total = 0
-    
+
     for batch_idx, (inputs, targets) in enumerate(trainloader):
-        if batch_idx %5 == 0 and batch_idx > 0:
+        if batch_idx % 5 == 0 and batch_idx > 0:
             print(str(batch_idx) + ' batch out of ' + str(data_length // bs) + ' elapse time since last epoch: ' +
-                str(time.time() - epoch_start) + ' training loss: ' + str((train_loss / (batch_idx + 1))))
+                  str(time.time() - epoch_start) + ' training loss: ' + str((train_loss / (batch_idx + 1))))
         targets = torch.tensor(np.squeeze(np.array(targets)), dtype=torch.long)
         inputs, targets = inputs.cuda(), targets.cuda()
         optimizer.zero_grad()
@@ -57,15 +59,16 @@ def train(epoch):
         _, predicted = torch.max(outputs.data, 1)
         total += targets.size(0)
         correct += predicted.eq(targets.data).cpu().sum().item()
-        
+
     Train_acc = 100. * correct / total
     print('Training ACC: ' + str(Train_acc))
+
 
 def validate(epoch):
     global val_acc
     global best_val_acc
     global best_val_acc_epoch
-    
+
     val_loss = 0
     correct = 0
     total = 0
@@ -95,13 +98,14 @@ def validate(epoch):
             'acc': val_acc,
             'epoch': epoch,
         }
-        torch.save(state,'slpstaging.v1')
+        torch.save(state, 'slpstaging.v1')
         best_val_acc = val_acc
         best_val_acc_epoch = epoch
 
+
 def testing():
     global test_acc
-    
+
     correct = 0
     total = 0
 
@@ -114,17 +118,23 @@ def testing():
         _, predicted = torch.max(outputs.data, 1)
         total += targets.size(0)
         correct += predicted.eq(targets.data).cpu().sum().item()
+        kappa = cohen_kappa_score(np.array(targets.data), np.array(predicted))
 
     val_acc = 100. * correct / total
     print('testing ACC: ' + str(val_acc))
+    print('kappa: ' + str(kappa))
+    return val_acc, kappa
 
-for fold in range(1):
+folds = 1
+for fold in range(folds):
     trainset = cpc(rdata, split='train', fold=fold, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=1)
     val_set = cpc(rdata, split='val', fold=fold, transform=transform_test)
     valloader = torch.utils.data.DataLoader(val_set, batch_size=bs, shuffle=False, num_workers=1)
     test_set = cpc(rdata, split='test', fold=fold, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(test_set, batch_size=bs, shuffle=False, num_workers=1)
+    testloader = torch.utils.data.DataLoader(test_set, batch_size=len(test_set), shuffle=False, num_workers=1)
+    acc_list = np.zeros([folds, 1])
+    kappa_list = np.zeros([folds, 1])
 
     # net = LiNet()
     net = VGG('VGG11')
@@ -137,9 +147,9 @@ for fold in range(1):
     if test_flg:
         checkpoint = torch.load('slpstaging.v1')
         net.load_state_dict(checkpoint['net'])
-        
+
     if test_flg:
-        testing()
+        acc_list[fold], kappa_list[fold] = testing()
     else:
         for epoch in range(total_epoch):
             train(epoch)
